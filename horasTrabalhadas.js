99a9dd016eab69faaafdc65a26e5bebfed9133ec
kelven mendes <97700347+kelvenmendes@users.noreply.github.com>
2023-02-02 16:48:36 -0300

    Add files via upload

---
+++ horasTrabalhadas.js
@@ -0,0 +1,44 @@
+
+//Função que calcula o salario liquido por horas trabalhadas
+
+
+
+
+function salario (qntHotas, recebe) {
+    qntHotas = a
+    recebe = b
+
+    b = 25
+    a = 8
+
+    function converte (){
+    }
+    
+    console.log(((b * a) * 20))  
+    
+}
+
+
+
+function calcularSalarioLiquido(horasTrabalhadas, ganhoPorHora) {
+    const salarioBruto = horasTrabalhadas * ganhoPorHora
+    const salarioLiquido = salarioBruto - salarioBruto * 30/100
+    return `Salário igual a R$ ${salarioLiquido}`
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
